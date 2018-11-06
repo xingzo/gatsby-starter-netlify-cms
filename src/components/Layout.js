a0214ddf2527515980382733eef7c148c14586e8
xingzo <kingsleynyaosi@gmail.com>
2018-11-06 01:05:51 -0600

    music page fix 1

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Afro-Extended | Extending Africa" />
     <Navbar />
     <div>{children}</div>
   </div>
