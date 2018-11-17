d66e119634c0f7bbc3f301eea6d1ef19bd7c5a2a
xingzo <kingsleynyaosi@gmail.com>
2018-11-16 19:25:29 -0600

    added music to homepage

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const HomePageTemplate = ({ title, content, contentComponent }) => {
+export const HomePageTemplate = ({ title, content, contentComponent, music }) => {
   const PageContent = contentComponent || Content
 
+  const loadLatestSongs = (posts) =>{
+    return (posts
+      .map(({ node: post }) => (
+        <div
+          className="content"
+          style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+          key={post.id}
+        >
+          <p>
+            <Link className="has-text-primary" to={post.fields.slug}>
+              {post.frontmatter.title}
+            </Link>
+            <span> &bull; </span>
+            <small>{post.frontmatter.date}</small>
+          </p>
+          <p>
+            {post.excerpt}
+            <br />
+            <br />
+            <Link className="button is-small" to={post.fields.slug}>
+              Keep Reading →
+            </Link>
+          </p>
+          <p>stupid </p>
+        </div>
+      )) )
+  }
+
   return (
+    <div>
     <section className="hero is-medium intro">
       <div className="hero-body">
           <div className="container">
@@ -46,6 +75,17 @@ export const HomePageTemplate = ({ title, content, contentComponent }) => {
           </div>
       </div>
     </section>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Latest Releases</h1>
+        <div class="columns">
+          {LatestReleases}
+          <div class="column">Auto {loadLatestSongs(music)}</div>
+          <div class="column is-three-fifths">is-three-fifths {loadActive()}</div>
+        </div>
+        </div>
+    </section>
+  </div>
   )
 }
 
@@ -53,14 +93,18 @@ HomePageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
+
 }
 
 const HomePage = ({ data }) => {
   const { markdownRemark: post } = data
+  const { edges: music } = data.allMarkdownRemark
+
 
   return (
     <Layout>
       <HomePageTemplate
+        music={music}
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
@@ -83,63 +127,22 @@ export const PageQuery = graphql`
         title
       }
     }
+    allMarkdownRemark(
+      limit: 3
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { templateKey: { eq: "music-post" } }}
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
   }
 `
-
-          // <PageContent classNameName="content" content={content} />
-
-          // <section className="hero is-medium intro">
-          //   <div className="hero-body">
-          //     <div className="container">
-          //       <div className="intro-columns">
-          //         <div className="intro-column is-content">
-          //           <div className="intro-content">
-          //             <h1 className="title intro-title">
-          //               <strong>Bulma</strong> is an open source CSS framework based on <strong>Flexbox</strong> and used by more than <strong>100,000</strong> developers.
-          //             </h1>
-          //
-          //             <div id="ghbtns" className="intro-ghbtns">
-          //               <iframe src="https://ghbtns.com/github-btn.html?user=jgthms&repo=bulma&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
-          //               <a href="https://www.npmjs.com/package/bulma" target="_blank" rel="nofollow">
-          //                 <img src="https://img.shields.io/npm/dm/bulma.svg" />
-          //               </a>
-          //             </div>
-          //
-          //             <pre id="npm" className="intro-npm">
-          //             <code id="npmCopy" className="intro-npm-copy" data-clipboard-text="{{ npmInstall }}">copy</code></pre>
-          //
-          //             <nav className="intro-buttons">
-          //               <a className="button is-primary is-large" href="{{ site.data.meta.download }}">
-          //                 <span>
-          //                   <strong>Download</strong>
-          //                   <PageContent classNameName="content" content={content} />
-          //
-          //                 </span>
-          //               </a>
-          //               <a className="button is-light is-large" href="{{ site.data.meta.documentation }}">
-          //                 <span>
-          //                   <span>View</span>
-          //                   <strong>docs</strong>
-          //                 </span>
-          //               </a>
-          //             </nav>
-          //           </div>
-          //         </div>
-          //
-          //         <div className="intro-column is-video">
-          //           <div id="introVideo" className="intro-video">
-          //             <div className="intro-shadow">youngg</div>
-          //             <div className="intro-spinner"></div>
-          //             <div className="intro-iframe">
-          //               <iframe id="introIframe" src="https://player.vimeo.com/video/237608586?color=00d1b2" width="640" height="338" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-          //             </div>
-          //           </div>
-          //           <p className="intro-author">
-          //             <span>Video by <a href="http://www.vuemastery.com/" target="_blank">Vue Mastery</a></span>
-          //           </p>
-          //         </div>
-          //       </div>
-          //
-          //     </div>
-          //   </div>
-          // </section>
