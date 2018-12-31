5147b6bf76b53f6f387dc2317fbfc69ac84834eb
xingzo <kingsleynyaosi@gmail.com>
2018-12-31 17:55:03 +0300

    changed tracklist to a dropdown

--- src/components/Tracklist.js
+++ src/components/Tracklist.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Tracklist = (tracklist) => {
+class Tracklist extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const dispenseTracklist = (tracklistStr) => {
+    this.state = {
+      showDropdown: true,
+    };
+  }
 
+  dispenseTracklist = (tracklistStr) => {
     let trackListArr = tracklistStr.split("\n");
     console.log(trackListArr)
 
@@ -13,30 +19,40 @@ const Tracklist = (tracklist) => {
         return <li>{track}</li>
       }
     })
-
     return <ul>{list}</ul>
+  }
+
+  openDropdown = (e) => {
+    if(this.state.showDropdown === false){
+      this.setState({ showDropdown: true });
+    }else{
+      this.setState({ showDropdown: false });
+    }
+    console.log(this.state.showDropdown);
 
   }
 
-  return (<div className="dropdown is-active" style={{"display":"block"}}>
-            <div className="dropdown-trigger">
-              <button className="button" aria-haspopup="true" aria-controls="dropdown-menu3">
-                <span>Tracklist</span>
-                <span className="icon is-small">
-                  <i className="fas fa-angle-down" aria-hidden="true"></i>
-                </span>
-              </button>
-            </div>
-            <div className="dropdown-menu" id="dropdown-menu3" role="menu" style={{"position":"relative"}}>
-              <div className="dropdown-content">
-                <div className="dropdown-item">
-                {dispenseTracklist(tracklist.tracklist)}
-                  <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
-                </div>
-              </div>
-            </div>
-          </div> )
+render (){
+  const dropdownClass = this.state.showDropdown ? "dropdown is-active" : "dropdown";
 
+  return <div className={dropdownClass} style={{"display":"block"}}>
+    <div className="dropdown-trigger" onClick={() => this.openDropdown() } style={{"text-align":"center"}}>
+    <hr/>
+      <button className="button" astyle={{"border-radius":"0", 'box-shadow': 'none'}}ria-haspopup="true" aria-controls="dropdown-menu3" >
+        <span>Tracklist</span>
+      </button>
+    <hr/>
+    </div>
+    <div className="dropdown-menu" id="dropdown-menu3" role="menu" >
+      <div className="dropdown-content" style={{"border-radius":"0", 'box-shadow': 'none'}}>
+        <div className="dropdown-item">
+          {this.dispenseTracklist(this.props.tracklist)}
+          <p>You can insert <strong>any type of content</strong> within the drmenu.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+}
 }
 
 export default Tracklist
