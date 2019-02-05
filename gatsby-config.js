2bac6dc58c2ab06ff004e1d0114fdb301569385f
xingzo <kingsleynyaosi@gmail.com>
2019-02-04 18:57:29 -0600

    trying gatsby-env-variables package

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,12 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
+      resolve: `gatsby-plugin-env-variables`,
+      options: {
+        whitelist: ["PAYPAL_PROD"]
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
